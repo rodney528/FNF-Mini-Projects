b1636c3279a65a1b59a2efb848c47563e8c48451
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2026-04-30 18:48:55 -0400

    Update Lua Utility Functions.lua
    * Cleaned up documentation.
    * Added renderer argument to textSplit.
    * Made prepImports use "..." instead of an argument.
    * Updated and added more example uses.

--- Psych/Lua Utility Functions.lua
+++ Psych/Lua Utility Functions.lua
@@ -1,38 +1,48 @@
--- Script by @rodney528
+----- [[ Script by @rodney528 ]] -----
 
--- Utility functions.
+----- [[ Potential Ideas ]] -----
+
+--[[
+
+	"_runHaxe..." functions.
+	"writeJson" function mayhaps?
+
+]]--
+
+----- [[ Utility Functions ]] -----
 
 ---Check's if the input is nil.
 ---@generic input
----@param variable any
----@param ifNil input
----@return input
+---@param variable any The data to check.
+---@param ifNil input What should be returned if nil.
+---@return input # The checked data.
 local function nilCheck(variable, ifNil)
 	return (type(variable) == 'nil' or variable == nil) and ifNil or variable
 end
 
 ---Check's if your running on v1 instances of Psych Engine.
 ---@param exact? boolean If true, it will look for v1.0.4 specifically.
----@return boolean
+---@return boolean # The results of the check.
 local function isNew(exact)
 	return nilCheck(exact, false) and version == '1.0.4' or version >= '1.0'
 end
 ---Check's if your running on v0.7 instances of Psych Engine.
 ---@param exact? boolean If true, it will look for v0.7.3 specifically.
----@return boolean
+---@return boolean # The results of the check.
 local function isLegacy(exact)
 	return nilCheck(exact, false) and version == '0.7.3' or (version <= '0.7.3' and version >= '0.7')
 end
 ---Check's if your running on v0.6 instances of Psych Engine.
 ---@param exact? boolean If true, it will look for v0.6.3 specifically.
----@return boolean
+---@return boolean # The results of the check.
 local function isBeta(exact)
 	return nilCheck(exact, false) and version == '0.6.3' or (version <= '0.6.3' and version >= '0.6')
 end
+-- it would be funny to add smth like "isOutdated" but nah, lol
 
 ---String interpolation in lua!
----@param ... any
----@return string
+---@param ... any The data to be interpolated.
+---@return string # The interpolated data.
 local function f(...)
 	---@param value table
 	---@return string
@@ -61,32 +71,31 @@ local function f(...)
 	return final
 end
 
----Split's a piece of string into an array.
----@param text string
----@param delimiter string
----@return string[]
-local function textSplit(text, delimiter)
+---Split's a piece of string into an array of your typing choice.
+---@generic input
+---@param text string The text to split.
+---@param delimiter string What to split by.
+---@param renderer? fun(index: integer, piece: string): input Allows you to customize how the data gets returned.
+---@return input[] # The split up content.
+local function textSplit(text, delimiter, renderer)
 	local splitTxt = stringSplit(text, delimiter) ---@type string[]
+	local finalArray = {} ---@type input[]
 	for index, value in pairs(splitTxt) do
-		splitTxt[index] = stringTrim(value)
+		if nilCheck(renderer, 'buh') == 'buh' then
+			table.insert(finalArray, stringTrim(value))
+		else
+			table.insert(finalArray, renderer(stringTrim(value)))
+		end
 	end
-	return splitTxt
+	return finalArray
 end
 
 ---Useful for prepping imports for runHaxeCode usage.
----
----## example:
----```lua
----runHaxeCode(f(
----	prepImports({'flixel.addons.display.FlxBackdrop'}),
----	[[ var ahh:FlxBackdrop = new FlxBackdrop(Paths.image('characters/BOYFRIEND')); ]]
----))
----```
----@param imports string[] The imports to prep.
----@return string
-local function prepImports(imports)
+---@param ... string[] The imports to prep.
+---@return string # If on v0.7 or higher, this returns the imports pre-prepped in the haxe language.
+local function prepImports(...)
 	local final = ''
-	for index, path in pairs(imports) do
+	for index, path in pairs({...}) do
 		if isBeta() then
 			runHaxeCode(f([[
 				var preppedImports:Array<String> = ']], path, [['.split('.');
@@ -102,28 +111,35 @@ local function prepImports(imports)
 end
 
 ---Checks if the charting mode is active.
----@return boolean
+---@return boolean # The state of charting mode.
 local function isChartingMode()
 	return getPropertyFromClass(f(isBeta() and '' or 'states.', 'PlayState'), 'chartingMode')
 end
 
----A shortcut function for debugPrint with some extra stuff to it.
+---A shortcut function for debugPrint, with some extra stuff to it.
+---@todo Add "color" argument.
 ---@param value any What you wish to debugPrint.
----@param isDebug? boolean If true, this will only print when in charting mode.
+---@param isDebug? boolean If true, this will only print when in charting mode or lua debug mode.
 local function trace(value, isDebug)
+	-- for eventual color support
+	local function code()
+		-- wrapped in "f" jic you pop a single table in here
+		debugPrint(f(value))
+	end
 	if nilCheck(isDebug, false) then
 		if isChartingMode() or luaDebugMode then
-			debugPrint(f(value))
+			code()
 		end
-	else -- wrapped in "f" jic you pop a single table in here
-		debugPrint(f(value))
+	else
+		code()
 	end
 end
 
 ---Returns the contents of a json file.
+---##### Thanks to my friend @atlasgamer27 for helping me figure this out! lol
 ---@param path string The file path.
 ---@param printWarning? boolean If true, it will print a warning if the file doesn't exist.
----@return table | nil
+---@return table | any[] | nil # The jsons contents.
 local function parseJson(path, printWarning)
 	local filePath = f(path, '.json')
 	local fileContents = ''
@@ -137,12 +153,9 @@ local function parseJson(path, printWarning)
 	end
 
 	runHaxeCode(f(
-		prepImports({'haxe.format.JsonParser'}),
-		[[ var fileContents:String = ']], fileContents, [[';
-		var jsonData = new JsonParser(fileContents).doParse();
-		setVar('jsonData_varHolder', jsonData); ]]
+		prepImports('haxe.format.JsonParser'),
+		[[ setVar('jsonData_varHolder', new JsonParser(']], fileContents, [[').doParse()); ]]
 	))
-
 	return getProperty('jsonData_varHolder')
 end
 
@@ -184,6 +197,7 @@ local function _setOnScripts(variable, value, ignoreSelf, exclusions, luaOnly)
 end
 
 ---Used to make callOnScripts usage compatible with older versions.
+---@todo Add a workaround for v0.7 always returning true.
 ---@param func string The function name.
 ---@param arguments? any[] The function arguments.
 ---@param ignoreStops? boolean Wether to ignore "Function_Stop" calls.
@@ -191,7 +205,7 @@ end
 ---@param excludedScripts? string[] Specific scripts to not call upon.
 ---@param excludedValues? any[] Values to prevent from being returned.
 ---@param luaOnly? boolean If true, it only calls callOnLuas when on newer versions.
----@return any returnValue Note: Always returns true on 0.7.3 for some reason? Might add a workaround, but I'm unsure atm.
+---@return any # Note: Always returns true on v0.7 for some reason? Might add a workaround, but I'm unsure atm.
 local function _callOnScripts(func, arguments, ignoreStops, ignoreSelf, excludedScripts, excludedValues, luaOnly)
 	arguments = nilCheck(arguments, {})
 	ignoreStops = nilCheck(ignoreStops, false)
@@ -210,28 +224,66 @@ local function _callOnScripts(func, arguments, ignoreStops, ignoreSelf, excluded
 end
 
 ---Checks if a property exists.
----@param variable string
----@return boolean
+---@param variable string The property to check.
+---@return boolean # Wether it exists.
 local function doesPropertyExist(variable)
 	local lol = getProperty(variable) ---@type any
 	return not (type(lol) == 'nil' or lol == variable)
 end
 
+----- [[ Example Uses ]] -----
 
-function onCreate()
-	-- easy and simple below v0.6 warning message :3
-	trace(f('Is New: ', isNew(true), ', Is Legacy: ', isLegacy(true), ', Is Beta: ', isBeta(true)), true)
-	if version < '0.6' then
-		trace(f(
-			'Hey this script only works on Psych v0.6 and above!\n',
-			'Psych v', version, ' isn\'t compatible with the script whatsoever!'
-		))
-		return close(true)
-	elseif not (isNew(true) or isLegacy(true) or isBeta(true)) then
-		trace(f(
-			'Hey this script might not work properly on Psych v', version, '!\n',
-			'If you wish for the script to work appropriately please use versions...\n',
-			'v0.6.3, v0.7.3 or v1.0.4! If the script works perfectly fine, then just ignore this message.'
-		), true)
-	end
+-- simple debug trace for version range
+trace(f(
+	'\nIs New: ', isNew(true), ' (v1.0.4)\n',
+	'Is Legacy: ', isLegacy(true), ' (v0.7.3)\n',
+	'Is Beta: ', isBeta(true), ' (v0.6.3)\n',
+	'Is Outdated: ', version < '0.6', ' (v0.5.2)'
+), true)
+
+-- below v0.6 warning message
+if version < '0.6' then
+	trace(f(
+		'\nHey, this script only works on Psych v0.6 and above!\n',
+		'Psych v', version, ' isn\'t compatible with the script whatsoever!'
+	))
+	return close(true)
+elseif not (isNew(true) or isLegacy(true) or isBeta(true)) then
+	trace(f(
+		'\nHey, this script might not work properly on Psych v', version, '!\n',
+		'If you wish for the script to work appropriately, please use versions...\n',
+		'v0.6.3, v0.7.3 or v1.0.4! If the script works perfectly fine, then just ignore this message.'
+	), true)
 end
+
+-- basic example on how to use prepImports
+runHaxeCode(f(
+	prepImports('flixel.addons.display.FlxBackdrop'),
+	[[ var ahh:FlxBackdrop = new FlxBackdrop(Paths.image('characters/BOYFRIEND')); ]]
+))
+-- maybe make the second arg be runHaxeCode?
+-- "_runHaxeCode" with prepImports built in?
+
+-- cool type definitioning
+local integers = textSplit('1, 2, 3', ',',
+	function (index, piece)
+		return math.floor(tonumber(piece))
+	end
+)
+trace(integers) -- returns "{1, 2, 3}"
+
+---@param index integer
+---@param piece string
+---@return number
+function sampleFunc(index, piece) return tonumber(piece) end
+local numbers = textSplit('12, 0.45, 0.3', ',', sampleFunc)
+trace(numbers) -- returns "{12, 0.45, 0.3}"
+
+-- JSON PARSING, BITCH!
+parseJson('characters/bf', true) -- if missing, prints "File not found: characters/bf.json"
+
+-- property existence check
+getProperty('gf.x') -- in older versions, this returns "gf.x"???
+getProperty('gf.x') -- in newer versions, this returns `nil`
+doesPropertyExist('gf.x') -- returns a bool
+-- way more consistent!
